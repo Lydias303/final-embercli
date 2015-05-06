776ad4010558e56a83f0f8398da5a76bbc65b796
Lydias303 <shaferlydia@yahoo.com>
2015-05-06 00:28:23 -0600

    talking to the api

--- app/models/todo.js
+++ app/models/todo.js
@@ -3,6 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean', {defaultValue: false}),
+  archived: DS.attr('boolean', {defaultValue: false}),
+  dueDate: DS.attr('date'),
+  description: DS.attr('string'),
 
   list: DS.belongsTo('list', {async: true})
 });
